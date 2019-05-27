7cf4bbf56209c229a9d6c8b5d0d23e1634f128d9
Marc Ziegler <marc.ziegler@uk-erlangen.de>
2019-05-27 17:27:48 +0200

    Bugfix in backup-function
    Fixed a bug in the backup-function.

--- dotFileConfig.sh
+++ dotFileConfig.sh
@@ -4,19 +4,19 @@ backup_existing_conf(){
     if ! [ -d .config_backup ]; then
         mkdir -p .config_backup/config
     fi
-    [ -f ${HOME}/.zshrc ] || mv --backup=numbered ${HOME}/.zshrc ${HOME}/.config_backup
-    [ -f ${HOME}/.bashrc ] || mv --backup=numbered ${HOME}/.bashrc ${HOME}/.config_backup
-    [ -f ${HOME}/.Xdefaults ] || mv --backup=numbered ${HOME}/.Xdefaults ${HOME}/.config_backup
-    [ -f ${HOME}/.bash_profile ] || mv --backup=numbered ${HOME}/.bash_profile ${HOME}/.config_backup
-    [ -f ${HOME}/.bash_exports ] || mv --backup=numbered ${HOME}/.bash_exports ${HOME}/.config_backup
-    [ -f ${HOME}/.bash_aliases ] || mv --backup=numbered ${HOME}/.bash_aliases ${HOME}/.config_backup
-    [ -f ${HOME}/.bash_funcs ] || mv --backup=numbered ${HOME}/.bash_funcs ${HOME}/.config_backup
-    [ -f ${HOME}/.profile ] || mv --backup=numbered ${HOME}/.profile ${HOME}/.config_backup
-    [ -d ${HOME}/.config/awesome ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
-    [ -d ${HOME}/.config/zsh ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
-    [ -d ${HOME}/.config/bash ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
-    [ -d ${HOME}/.urxvt ] || mv --backup=numbered ${HOME}/.urxvt ${HOME}/.config_backup/
-    [ -d ${HOME}/.oh-my-zsh ] || mv --backup=numbered ${HOME}/.oh-my-zsh ${HOME}/.config_backup/
+    [ ! -f ${HOME}/.zshrc ] || mv --backup=numbered ${HOME}/.zshrc ${HOME}/.config_backup
+    [ ! -f ${HOME}/.bashrc ] || mv --backup=numbered ${HOME}/.bashrc ${HOME}/.config_backup
+    [ ! -f ${HOME}/.Xdefaults ] || mv --backup=numbered ${HOME}/.Xdefaults ${HOME}/.config_backup
+    [ ! -f ${HOME}/.bash_profile ] || mv --backup=numbered ${HOME}/.bash_profile ${HOME}/.config_backup
+    [ ! -f ${HOME}/.bash_exports ] || mv --backup=numbered ${HOME}/.bash_exports ${HOME}/.config_backup
+    [ ! -f ${HOME}/.bash_aliases ] || mv --backup=numbered ${HOME}/.bash_aliases ${HOME}/.config_backup
+    [ ! -f ${HOME}/.bash_funcs ] || mv --backup=numbered ${HOME}/.bash_funcs ${HOME}/.config_backup
+    [ ! -f ${HOME}/.profile ] || mv --backup=numbered ${HOME}/.profile ${HOME}/.config_backup
+    [ ! -d ${HOME}/.config/awesome ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
+    [ ! -d ${HOME}/.config/zsh ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
+    [ ! -d ${HOME}/.config/bash ] || mv --backup=numbered ${HOME}/.config/awesome ${HOME}/.config_backup/config/
+    [ ! -d ${HOME}/.urxvt ] || mv --backup=numbered ${HOME}/.urxvt ${HOME}/.config_backup/
+    [ ! -d ${HOME}/.oh-my-zsh ] || mv --backup=numbered ${HOME}/.oh-my-zsh ${HOME}/.config_backup/
 }
 
 
@@ -28,8 +28,9 @@ exists=0
 ### create a backup of the existing configuration and clone my dotfiles from github ###
 [ $exists -eq "0" ] \
     && backup_existing_conf \
-    && git clone --bare https://www.github.com/zieglemc/Dotfiles.git $HOME/.dotfileconf \
-    && git --git-dir=$HOME/.dotfileconf/ --work-tree=$HOME config --local status.showUntrackedFiles no
+    && git clone --recursive --bare https://www.github.com/zieglemc/Dotfiles.git $HOME/.dotfileconf \
+    && git --git-dir=$HOME/.dotfileconf/ --work-tree=$HOME config --local status.showUntrackedFiles no \
+    && git --git-dir=$HOME/.dotfileconf/ --work-tree=$HOME checkout
 
 ### installing fzf ###
 [ -d ${HOME}/.fzf ] || ( git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install )
